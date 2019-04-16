225a1a210c1b96b0ea709f662a6f1d9fa0b5fbe6
mrtillman <askmrtillman@gmail.com>
2019-04-16 19:23:15 -0400

    create test project; start unit tests for CounterCulture.Services; update readme

--- README.md
+++ README.md
@@ -36,37 +36,3 @@ dotnet run
 ```
 
 Open [http://localhost:5000](http://localhost:5000) to view it in the browser. Upon successful login, the page will redirect to http://localhost:8080, so be sure [counter-culture.app](https://github.com/mrtillman/counter-culture.app) is up and running on port 8080.
-
-## Deployment
-
-Sign in to the remote server and clone the repo:
-
-```sh
-ssh username@counter-culture.io
-cd ~/auth
-git clone https://github.com/mrtillman/counter-culture.auth.git
-```
-
-Remember to create `appsecrets.json`.
-
-Next, use the production launch settings:
-
-```sh
-cd Properties/
-mv launchSettings.prod.json launchSettings.json
-cd ..
-```
-
-Containerize the auth server:
-
-```sh
-docker build -t counter-culture.auth .
-```
-
-Start the container on the remote production server:
-
-```sh
-docker run -p 5000:5000 -d username/counter-culture.auth
-```
-
-For more details, see the [Frequently used Docker CLI commands](https://github.com/mrtillman/counter-culture.docs/blob/master/docker/cli-commands.md).
---
+++ Tests/Services/UserService.Find.cs
@@ -0,0 +1,40 @@
+using Moq;
+using System;
+using CounterCulture.Services;
+using CounterCulture.Repositories;
+using Microsoft.Extensions.Options;
+using CounterCulture.Repositories.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class UserService_Find
+    {
+        private UserService service;
+
+        public UserService_Find() {
+          var repoMock = new Mock<IUserRepository>(MockBehavior.Strict);
+          var optionsMock = new Mock<IOptions<AppSecrets>>();
+          repoMock.Setup(mock => mock.Find(It.IsAny<String>(), It.IsAny<String>()))
+                  .Returns(new User());
+          service = new UserService(repoMock.Object, optionsMock.Object);
+        }
+
+        [TestMethod]
+        public void ShouldGetUser()
+        {
+            String username = "waldo";
+            String password = "waldo's password";
+            User user = service.Find(username, password);
+            Assert.IsNotNull(user);
+        }
+
+        [TestMethod]
+        public void ShouldReturnNull_WhenCredentialsNullOrEmpty()
+        {
+            User user = service.Find("", "");
+            Assert.IsNull(user);
+        }
+    }
+}
