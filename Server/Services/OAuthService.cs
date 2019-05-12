034c96c41ec02f5bf8d0b34c6d5bb4b4861a5155
mrtillman <askmrtillman@gmail.com>
2019-05-12 01:04:28 -0400

    add logic to generate client id and secret; update oauth client repo to include app type, name, description and homepage; remove hex helper

--- Server/Services/OAuthService.cs
+++ Server/Services/OAuthService.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Text;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using CounterCulture.Repositories;
 using CounterCulture.Repositories.Models;
+using CounterCulture.Secure.Helpers;
 
 namespace CounterCulture.Services {
     public class OAuthService : IOAuthService
@@ -14,12 +16,27 @@ namespace CounterCulture.Services {
         public IOAuthRepository OAuthRepo { get; set; }
 
         public OAuthClient RegisterClient(OAuthClient client) {
-            client.client_id = "generate client_id";
-            client.client_secret = "generate client_secret";
+            client.client_id = _generateClientId();
+            client.client_secret = _generateClientSecret();
             if(OAuthRepo.SaveOAuthClient(client)){
                 return client;
             }
             return null;
+        }
+
+        private string _generateClientId() {
+            var clientId = Guid.NewGuid().ToString();
+            return _hexEncode(clientId);
+        }
+
+        private string _generateClientSecret() {
+            return SHA256Hash.Compute(_generateClientId());
+        }
+
+        private string _hexEncode(string input){
+            var bytes = Encoding.Default.GetBytes(input);
+            var hexString = BitConverter.ToString(bytes);
+            return hexString.Replace("-","");
         }
     }
 }
