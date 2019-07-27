a9446f3c7ce52c8298230b0645e7b790a4bbe5a7
mrtillman <askmrtillman@gmail.com>
2019-07-27 18:29:06 -0400

    suppress code that may be soon deprecated

--- Server/Controllers/OAuth2Controller.cs
+++ Server/Controllers/OAuth2Controller.cs
@@ -1,95 +1,95 @@
-﻿using System;
-using System.Web;
-using System.Web.Http;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Principal;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Logging;
-using CounterCulture.Services;
-using CounterCulture.Constants;
-using CounterCulture.Models;
-using CounterCulture.Utilities;
-using Microsoft.AspNetCore.Mvc.Versioning;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+﻿// using System;
+// using System.Web;
+// using System.Web.Http;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Security.Principal;
+// using Microsoft.AspNetCore.Mvc;
+// using Microsoft.AspNetCore.Authorization;
+// using Microsoft.AspNetCore.Hosting;
+// using Microsoft.Extensions.Logging;
+// using CounterCulture.Services;
+// using CounterCulture.Constants;
+// using CounterCulture.Models;
+// using CounterCulture.Utilities;
+// using Microsoft.AspNetCore.Mvc.Versioning;
+// using Microsoft.AspNetCore.Authentication.JwtBearer;
 
-namespace CounterCulture.Controllers
-{
+// namespace CounterCulture.Controllers
+// {
     
-    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-    public class OAuth2Controller : BaseController
-    {
-        public OAuth2Controller(
-            ICacheService CacheService,
-            ILogger<OAuth2Controller> LoggerService,
-            IOAuthService OAuthService)
-            :base(CacheService)
-        {
-            OAuth = OAuthService;
-            Logger = LoggerService;
-        }
+//     [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+//     public class OAuth2Controller : BaseController
+//     {
+//         public OAuth2Controller(
+//             ICacheService CacheService,
+//             ILogger<OAuth2Controller> LoggerService,
+//             IOAuthService OAuthService)
+//             :base(CacheService)
+//         {
+//             OAuth = OAuthService;
+//             Logger = LoggerService;
+//         }
 
-        private IOAuthService OAuth { get; set; }
-        private ILogger<OAuth2Controller> Logger { get; set; }
+//         private IOAuthService OAuth { get; set; }
+//         private ILogger<OAuth2Controller> Logger { get; set; }
 
-        [HttpGet]
-        public ActionResult<OAuthClient> Get() {
-            if (!User.Identity.IsAuthenticated){
-                return Unauthorized();
-            }
-            var claim = User.Claims.ElementAt(0);
-            var client_id = claim.Value;
-            return Ok(OAuth.GetClient(client_id));
-        }
+//         [HttpGet]
+//         public ActionResult<OAuthClient> Get() {
+//             if (!User.Identity.IsAuthenticated){
+//                 return Unauthorized();
+//             }
+//             var claim = User.Claims.ElementAt(0);
+//             var client_id = claim.Value;
+//             return Ok(OAuth.GetClient(client_id));
+//         }
 
-        [HttpPost]
-        [AllowAnonymous] // TODO: remove - require geek site token
-        [Route("register")]
-        public ActionResult Register([FromBody] OAuthClient client) {
-            Logger.LogInformation(LoggingEvents.RegisterApp, client.app_name);
-            return Ok(OAuth.RegisterClient(client));
-        }
+//         [HttpPost]
+//         [AllowAnonymous] // TODO: remove - require geek site token
+//         [Route("register")]
+//         public ActionResult Register([FromBody] OAuthClient client) {
+//             Logger.LogInformation(LoggingEvents.RegisterApp, client.app_name);
+//             return Ok(OAuth.RegisterClient(client));
+//         }
 
-        [HttpPost]
-        [AllowAnonymous]
-        [Route("access_token")]
-        public ActionResult AccessToken([FromQuery] AuthRequest authReq)
-        {
+//         [HttpPost]
+//         [AllowAnonymous]
+//         [Route("access_token")]
+//         public ActionResult AccessToken([FromQuery] AuthRequest authReq)
+//         {
 
-            // TODO: check authReq.grant_type
+//             // TODO: check authReq.grant_type
 
-            if(String.IsNullOrWhiteSpace(authReq.code)){
-                return Unauthorized();
-            }
-            var authCacheValue = Cache.Get(authReq.code);
-            if(String.IsNullOrWhiteSpace(authCacheValue)){
-                return Unauthorized();
-            }
+//             if(String.IsNullOrWhiteSpace(authReq.code)){
+//                 return Unauthorized();
+//             }
+//             var authCacheValue = Cache.Get(authReq.code);
+//             if(String.IsNullOrWhiteSpace(authCacheValue)){
+//                 return Unauthorized();
+//             }
 
-            Cache.Delete(authReq.code);
+//             Cache.Delete(authReq.code);
             
-            // TODO: simplify using string extensions
-            var authParts = authCacheValue.Split(':');
-            var clientId = authParts[0];
-            var userId = authParts[1];
-            var redirect_uri = HttpUtility.UrlDecode(authReq.redirect_uri);
+//             // TODO: simplify using string extensions
+//             var authParts = authCacheValue.Split(':');
+//             var clientId = authParts[0];
+//             var userId = authParts[1];
+//             var redirect_uri = HttpUtility.UrlDecode(authReq.redirect_uri);
 
-            var client = OAuth.FindClient(
-                            clientId, 
-                            authReq.client_secret, 
-                            redirect_uri);
-            if(client == null){
-                return Unauthorized();
-            }
-            client.user_id = userId;
-            var authResponse = OAuth.Authenticate(client);
-            if(authResponse == null){
-                return Unauthorized();
-            }
-            return Ok(authResponse);
-        }
+//             var client = OAuth.FindClient(
+//                             clientId, 
+//                             authReq.client_secret, 
+//                             redirect_uri);
+//             if(client == null){
+//                 return Unauthorized();
+//             }
+//             client.user_id = userId;
+//             var authResponse = OAuth.Authenticate(client);
+//             if(authResponse == null){
+//                 return Unauthorized();
+//             }
+//             return Ok(authResponse);
+//         }
 
-    }
-}
+//     }
+// }
--- Server/Services/IOAuthService.cs
+++ Server/Services/IOAuthService.cs
@@ -1,14 +1,14 @@
-using System;
-using System.Collections.Generic;
-using CounterCulture.Models;
+// using System;
+// using System.Collections.Generic;
+// using CounterCulture.Models;
 
-namespace CounterCulture.Services {
-    public interface IOAuthService
-    {
-        AuthResponse Authenticate(OAuthClient client);
-        OAuthClient GetClient(string client_id);
-        OAuthClient FindClient(
-            string client_id, string client_secret, string redirect_uri);
-        OAuthClient RegisterClient(OAuthClient client);
-    }
-}
+// namespace CounterCulture.Services {
+//     public interface IOAuthService
+//     {
+//         AuthResponse Authenticate(OAuthClient client);
+//         OAuthClient GetClient(string client_id);
+//         OAuthClient FindClient(
+//             string client_id, string client_secret, string redirect_uri);
+//         OAuthClient RegisterClient(OAuthClient client);
+//     }
+// }
