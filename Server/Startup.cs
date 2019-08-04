362ddd01c4e32b8dc3bc855d435f64c85b612e92
mrtillman <askmrtillman@gmail.com>
2019-08-04 12:59:14 -0400

    simplify startup - isolate configuration for app cookie and app authentication

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -1,21 +1,11 @@
-﻿using System;
-using System.Text;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using CounterCulture.Repositories;
-using CounterCulture.Services;
 using CounterCulture.Constants;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json;
-using Microsoft.AspNetCore.Identity;
-using IdentityServer4.Test;
-using System.IdentityModel.Tokens.Jwt;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using CounterCulture.Configuration;
 
 namespace CounterCulture
@@ -36,24 +26,15 @@ namespace CounterCulture
             }
         }
 
+        private string appSecret {
+            get {
+                return Configuration["AppSecret"];
+            }
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
-            services.ConfigureApplicationCookie(options =>
-            {
-                // Cookie settings
-                options.Cookie.HttpOnly = true;
-                options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
-                options.LoginPath = "/account/login";
-                options.AccessDeniedPath = "/";
-                options.SlidingExpiration = true;
-            });
 
             services.AddApiVersioning();
 
@@ -62,39 +43,9 @@ namespace CounterCulture
                         options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
                     });
             
-            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+            services.ConfigureAppCookieAndPolicy();
 
-            var signingKey = Encoding.ASCII
-                            .GetBytes(Configuration["AppSecret"]);
-            
-            var _tokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(signingKey),
-                    ValidateIssuer = true,
-                    ValidIssuer = env.IsDevelopment() 
-                                  ? ServerUrls.SECURE[ENV.DEV] 
-                                  : ServerUrls.SECURE[ENV.PROD],
-                    ValidateAudience = true,
-                    ValidAudience = env.IsDevelopment() 
-                                  ? $"{ServerUrls.SECURE[ENV.DEV]}/resources" 
-                                  : $"{ServerUrls.SECURE[ENV.PROD]}/resources",
-                };
-
-            services.AddAuthentication()
-            .AddCookie(options => options.SlidingExpiration = true)
-            .AddJwtBearer(options =>
-            {
-                options.Authority = env.IsDevelopment() 
-                                  ? ServerUrls.SECURE[ENV.DEV] 
-                                  : ServerUrls.SECURE[ENV.PROD];
-                options.Audience = env.IsDevelopment() 
-                                  ? $"{ServerUrls.SECURE[ENV.DEV]}/resources" 
-                                  : $"{ServerUrls.SECURE[ENV.PROD]}/resources";
-                options.RequireHttpsMetadata = env.IsProduction();
-                options.SaveToken = true;
-                options.TokenValidationParameters = _tokenValidationParameters;
-            });
+            services.ConfigureAuthentication(appSecret, env.IsProduction() ? ENV.PROD : ENV.DEV);
 
             services.ConfigureAspNetIdentity(mySqlConnectionString);
 
