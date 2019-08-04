5c7d484348eaebb50396d99ae410648a7db6f6a2
mrtillman <askmrtillman@gmail.com>
2019-08-04 18:09:45 -0400

    rename + add comments

--- Server/Configuration/securedb.create.sql
+++ Server/Configuration/securedb.create.sql
@@ -3,11 +3,23 @@ CREATE DATABASE secure;
 USE secure;
 
 -- root user for admin access
-ALTER USER 'root' IDENTIFIED BY 'password';
+ALTER USER 'root' IDENTIFIED BY '{{root_password}}';
 GRANT ALL PRIVILEGES ON secure.* TO 'root'@'%';
 
 -- user for entity framework connection
-CREATE USER 'username'@'%' IDENTIFIED BY 'password';
+CREATE USER '{{username}}'@'%' IDENTIFIED BY '{{password}}';
 
-ALTER USER 'username' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON secure.* TO 'username'@'%';
+ALTER USER '{{username}}' IDENTIFIED BY '{{password}}';
+GRANT ALL PRIVILEGES ON secure.* TO '{{username}}'@'%';
+
+/*
+
+to, seed this database:
+0. open terminal
+1. cd to counter-culture/secure/Server
+2. run each:
+    o. dotnet ef database update -c ConfigurationDbContext
+    i. dotnet ef database update -c PersistedGrantDbContext
+   ii. dotnet ef database update -c SecureDbContext
+
+*/
