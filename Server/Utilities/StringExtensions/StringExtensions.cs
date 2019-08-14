200a14b0b66eb36d2c114d1d8801358199130c9a
mrtillman <askmrtillman@gmail.com>
2019-08-13 22:57:40 -0400

    add endpoint to handle client registration; create registrtation view model; restore string constants

---
+++ Server/Utilities/StringExtensions/StringExtensions.cs
@@ -0,0 +1,36 @@
+
+using System;
+using System.Text;
+using System.Security.Cryptography;
+using CounterCulture.Utilities;
+
+namespace CounterCulture.Utilities
+{
+
+  public static class StringExtensions
+  {
+    public static string NewClientId(this string stringValue){
+      var buffer = getRandomBytes(32);
+      return Convert.ToBase64String(buffer).HexEncode();
+    }
+
+    public static string NewClientSecret(this string stringValue){
+      var buffer = getRandomBytes(64);
+      var suffix = Convert.ToBase64String(buffer).HexEncode();
+      return $"secret.{suffix.ToLower()}";
+    }
+    public static string HexEncode(this string stringValue){
+            var bytes = Encoding.Default.GetBytes(stringValue);
+            var hexString = BitConverter.ToString(bytes);
+            return hexString.Replace("-","");
+    }
+
+    private static byte[] getRandomBytes(int length) {
+      RandomNumberGenerator rng = RandomNumberGenerator.Create();
+      byte[] buffer = new byte[length];
+      rng.GetBytes(buffer);
+      return buffer;
+    }
+  }
+
+}
