e85c47a55bd38b47075c92ec23ccfcb0e6ac4f68
mrtillman <askmrtillman@gmail.com>
2019-08-18 22:53:59 -0400

    revise comments

--- Server/Configuration/securedb.create.sql
+++ Server/Configuration/securedb.create.sql
@@ -1,12 +1,13 @@
 CREATE DATABASE secure;
 
+-- database for the DefaultMySQLConnection from appsettings.json
 USE secure;
 
 -- root user for admin access
 ALTER USER 'root' IDENTIFIED BY '{{root_password}}';
 GRANT ALL PRIVILEGES ON secure.* TO 'root'@'%';
 
--- user for entity framework connection
+-- user for the DefaultMySQLConnection from appsettings.json
 CREATE USER '{{username}}'@'%' IDENTIFIED BY '{{password}}';
 
 ALTER USER '{{username}}' IDENTIFIED BY '{{password}}';
@@ -14,7 +15,7 @@ GRANT ALL PRIVILEGES ON secure.* TO '{{username}}'@'%';
 
 /*
 
-to, seed this database:
+to seed the database:
 0. open terminal
 1. cd to counter-culture/secure/Server
 2. run each:
