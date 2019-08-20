d25e05a3f0b85baa6232c18d20a39d6bd42bba30
mrtillman <askmrtillman@gmail.com>
2019-08-19 21:51:04 -0400

    add logic to print client info to the console

--- Server/Utilities/DataSeed/DataSeed.Clients.cs
+++ Server/Utilities/DataSeed/DataSeed.Clients.cs
@@ -22,42 +22,75 @@ namespace CounterCulture.Utilities
                 };
       }
     }
+
     public static IEnumerable<Client> Clients
-      {
-        get
-        {
-          return new List<Client> {
-              new Client {
-                  AccessTokenType = AccessTokenType.Jwt,
-                  AccessTokenLifetime = 86400,
-                  AllowedScopes = _allowedScopes,
-                  AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
-                  ClientId = "countercultureapp",
-                  ClientName = "counter-culture.app",
-                  ClientSecrets = new List<Secret> {
-                      new Secret("superSecretPassword".Sha512())},
-                  RedirectUris = {
-                    $"{ServerUrls.APP[ENV.DEV]}/oauth2/callback",
-                    $"{ServerUrls.APP[ENV.PROD]}/oauth2/callback"
-                  }
-              },
-              new Client {
-                  AccessTokenType = AccessTokenType.Jwt,
-                  AccessTokenLifetime = 86400,
-                  AllowedScopes = _allowedScopes,
-                  AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
-                  ClientId = "counterculturedev",
-                  ClientName = "counter-culture.dev",
-                  ClientSecrets = new List<Secret> {
-                      new Secret("superSecretPassword".Sha512())},
-                  RedirectUris = {
-                    $"{ServerUrls.DEV[ENV.DEV]}/oauth2/callback",
-                    $"{ServerUrls.DEV[ENV.PROD]}/oauth2/callback"
-                  }
-              }
-          };
-        }
+    {
+      get
+      { 
+
+        PrintClientInfo(CounterCultureAppInfo);
+        
+        PrintClientInfo(CounterCultureDevInfo);
+
+        return new List<Client> {
+            new Client {
+                AccessTokenType = AccessTokenType.Jwt,
+                AccessTokenLifetime = 86400,
+                AllowedScopes = _allowedScopes,
+                AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
+                ClientName = CounterCultureAppInfo.Item1,
+                ClientId = CounterCultureAppInfo.Item2,
+                ClientSecrets = new List<Secret> {
+                    new Secret(CounterCultureAppInfo.Item3.Sha512())},
+                RedirectUris = {
+                  $"{ServerUrls.APP[ENV.DEV]}/oauth2/callback",
+                  $"{ServerUrls.APP[ENV.PROD]}/oauth2/callback"
+                }
+            },
+            new Client {
+                AccessTokenType = AccessTokenType.Jwt,
+                AccessTokenLifetime = 86400,
+                AllowedScopes = _allowedScopes,
+                AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
+                ClientName = CounterCultureDevInfo.Item1,
+                ClientId = CounterCultureDevInfo.Item2,
+                ClientSecrets = new List<Secret> {
+                    new Secret(CounterCultureDevInfo.Item3.Sha512())},
+                RedirectUris = {
+                  $"{ServerUrls.DEV[ENV.DEV]}/oauth2/callback",
+                  $"{ServerUrls.DEV[ENV.PROD]}/oauth2/callback"
+                }
+            }
+        };
       }
+    }
+
+    private static void PrintClientInfo(Tuple<string, string, string> clientInfo){
+      var defaultColor = Console.ForegroundColor;
+      Console.ForegroundColor = ConsoleColor.DarkMagenta;
+      Console.WriteLine($"\nClientName: {clientInfo.Item1}");
+      Console.WriteLine($"ClientId: {clientInfo.Item2}");
+      Console.WriteLine($"ClientSecret: {clientInfo.Item3}\n");
+      Console.ForegroundColor = defaultColor;
+    }
+
+    private static Tuple<string, string, string> CounterCultureAppInfo {
+      get {
+        return new Tuple<string, string, string>(
+            "counter-culture.app",
+            String.Empty.NewClientId(),
+            String.Empty.NewClientSecret());
+      }
+    }
+
+    private static Tuple<string, string, string> CounterCultureDevInfo {
+      get {
+        return new Tuple<string, string, string>(
+            "counter-culture.dev",
+            String.Empty.NewClientId(),
+            String.Empty.NewClientSecret());
+      }
+    }
   }
 
 }
