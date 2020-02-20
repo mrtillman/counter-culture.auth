4bf4fc3d314464b50142d65b53e2b7a58d9c0b59
mrtillman <askmrtillman@gmail.com>
2020-02-19 21:58:15 -0500

    remove unnecessary namespaces

--- Presentation/Controllers/UsersController.cs
+++ Presentation/Controllers/UsersController.cs
@@ -1,29 +1,16 @@
-﻿using System;
-using System.Web.Http;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Principal;
-using System.Security.Claims;
+﻿using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Logging;
-using Presentation.Services;
-using Presentation.Constants;
-using Presentation.Models;
-using Presentation.Utilities;
-using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using AspNet.Security.OpenIdConnect.Extensions;
 using AspNet.Security.OpenIdConnect.Primitives;
-using AspNet.Security.OpenIdConnect.Server;
 
 namespace Presentation.Controllers
 {
-    
-    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+
+  [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class UsersController : BaseController
     {
         public UsersController(
