91e79c778dd3d61e069a843679cb1c3d74d024fd
Stephen Tillman <askmrtillman@gmail.com>
2020-04-04 11:46:38 -0400

    fix typo

--- get-token.sh
+++ get-token.sh
@@ -4,6 +4,6 @@ curl -X POST \
 -F 'grant_type=client_credentials' \
 -F 'client_id='$CLIENT_ID'' \
 -F 'client_secret='$CLIENT_SECRET'' \
--F 'redirect_uri='$REDIRET_URI'' $AUTH_URL
+-F 'redirect_uri='$REDIRECT_URI'' $AUTH_URL
 
 echo " "
